99bfadfa3e32b58d5c7973ba591ef71c92cce6f6
devoil <devdevoildev@gmail.com>
2025-01-07 14:31:49 +0900

    ✨ Feat: [didact] step 1: createElement 함수

--- react-didact/index.js
+++ react-didact/index.js
@@ -1,18 +1,36 @@
-const element = {
-  type: 'h1',
-  props: {
-    title: 'foo',
-    children: '텍스트를 입력해보자!',
-  },
+function createElement(type, props, ...children) {
+  return {
+    type,
+    props: {
+      ...props,
+      children: children.map((child) =>
+        typeof child === 'object' ? child : createTextElement(child)
+      ),
+    },
+  };
+}
+
+function createTextElement(text) {
+  return {
+    type: 'TEXT_ELEMENT',
+    props: {
+      nodeValue: text,
+      children: [],
+    },
+  };
+}
+
+const Didact = {
+  createElement,
 };
 
+/** @jsx Didact.createElement */
+const element = (
+  <div id='foo'>
+    <a>bar</a>
+    <b />
+  </div>
+);
+
 const container = document.getElementById('root');
-
-const node = document.createElement(element.type);
-node['title'] = element.props.title;
-
-const text = document.createTextNode('');
-text['nodeValue'] = element.props.children;
-
-node.appendChild(text);
-container.appendChild(node);
+ReactDOM.render(element, container);
