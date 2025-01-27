cd9d8dc0ddec3a680873b75a84203cb1a05949fa
devoil <devdevoildev@gmail.com>
2025-01-27 22:57:34 +0900

    ✨ Feat: [react-basic] 로딩, 에러 상태 추가

--- react-basic/src/basic/components/Products.jsx
+++ react-basic/src/basic/components/Products.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react';
 
 export default function Products() {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState();
   const [products, setProducts] = useState([]);
   const [checked, setChecked] = useState(false);
   const handleChange = () => setChecked((prev) => !prev);
@@ -8,19 +10,28 @@ export default function Products() {
   // 컴포넌트가 처음등록되었을 때 처리해야하는 일이 있다면, useEffect 사용.
   // 사용하지 않는다면? 무한 루프에 빠지게 된다.
   useEffect(() => {
+    setLoading(true);
+    setError(undefined);
+
     // 컴포넌트가 마운트 되면 처음에만 호출
     fetch(`data/${checked ? 'sale_' : ''}products.json`) // checked 값에 따라 다른 데이터를 가져옴
       .then((res) => res.json())
       .then((data) => {
         console.log('🔥 뜨끈한 데이터를 네트워크에서 받아옴!');
         setProducts(data);
-      });
+      })
+      .catch((e) => setError('에러가 발생했음!'))
+      .finally(() => setLoading(false));
 
     return () => {
       // 컴포넌트가 언마운트 되면
       console.log('🧹 깨끗하게 청소하는 일들을 합니다');
     };
   }, [checked]); // checked 값이 변경될 때만 호출
+
+  if (loading) return <p>Loading...</p>;
+
+  if (error) return <p>{error}</p>;
 
   return (
     <>
