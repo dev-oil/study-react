32bdcfa1d802a1ede55d708efbdd442c9036d18f
devoil <devdevoildev@gmail.com>
2025-01-31 17:46:33 +0900

    ✨ Feat: [react-basic] react router 사용해보기

--- react-basic/src/App.js
+++ react-basic/src/App.js
@@ -1,7 +1,25 @@
 import React from 'react';
+import { RouterProvider } from 'react-router';
+import { createBrowserRouter } from 'react-router-dom';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <p>Home</p>,
+    errorElement: (
+      <>
+        <p>Not Found 🥹</p>
+      </>
+    ),
+  },
+  {
+    path: '/videos',
+    element: <p>Videos</p>,
+  },
+]);
 
 function App() {
-  return <></>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
