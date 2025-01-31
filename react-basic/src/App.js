a7c6d4824becc7cd9381f554e0bb9a412efd1985
devoil <devdevoildev@gmail.com>
2025-01-31 17:31:05 +0900

    🚚 Rename: [react-basic] 폴더 정리 및 react router 설치

--- react-basic/src/App.js
+++ react-basic/src/App.js
@@ -1,18 +1,7 @@
 import React from 'react';
-import Button1 from './components/Button1';
-import Button2 from './components/Button2';
-import StyledComponent from './StyledComponent';
-import TailwindComponent from './TailwindComponent';
 
 function App() {
-  return (
-    <>
-      <Button1 />
-      <Button2 />
-      <StyledComponent />
-      <TailwindComponent />
-    </>
-  );
+  return <></>;
 }
 
 export default App;
