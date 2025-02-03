80779d4b06d1e388281958a9584a84c4338af18d
devoil <devdevoildev@gmail.com>
2025-02-03 17:18:47 +0900

    ✨ Feat: [react-basic] react query 사용해보기

--- react-basic/src/App.js
+++ react-basic/src/App.js
@@ -1,7 +1,15 @@
 import React from 'react';
 import './App.css';
 import MainProducts from './components/MainProducts';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+// Create a client
+const queryClient = new QueryClient();
 
 export default function App() {
-  return <MainProducts />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <MainProducts />
+    </QueryClientProvider>
+  );
 }
--- react-basic/src/components/Products.jsx
+++ react-basic/src/components/Products.jsx
@@ -1,12 +1,24 @@
 import React, { useState } from 'react';
 import useProducts from '../hooks/use-products';
+import { useQueries, useQuery } from '@tanstack/react-query';
 
 export default function Products() {
   const [checked, setChecked] = useState(false);
-  const [loading, error, products] = useProducts({ salesOnly: checked });
+  const {
+    isLoading,
+    error,
+    data: products,
+  } = useQuery({
+    queryKey: ['products'],
+    queryFn: async () => {
+      console.log('fetching...');
+      return fetch(`data/products.json`).then((res) => res.json());
+    },
+  });
+  // const [loading, error, products] = useProducts({ salesOnly: checked });
   const handleChange = () => setChecked((prev) => !prev);
 
-  if (loading) return <p>Loading...</p>;
+  if (isLoading) return <p>Loading...</p>;
 
   if (error) return <p>{error}</p>;
 
