6de8444d8f619ce62d9e6deeac4177d427be6a4c
devoil <devdevoildev@gmail.com>
2025-02-03 13:58:42 +0900

    ✨ Feat: [react-basic] custom hook 문제점 살펴보기

--- react-basic/src/App.js
+++ react-basic/src/App.js
@@ -1,27 +1,7 @@
 import React from 'react';
-import { RouterProvider } from 'react-router';
-import { createBrowserRouter } from 'react-router-dom';
-import Home from './pages/Home';
-import NotFound from './pages/NotFound';
-import Root from './pages/Root';
-import Videos from './pages/Videos';
-import VideoDetail from './pages/VideoDetail';
+import './App.css';
+import MainProducts from './components/MainProducts';
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Root />,
-    errorElement: <NotFound />,
-    children: [
-      { index: true, element: <Home /> },
-      { path: 'videos', element: <Videos /> },
-      { path: 'videos/:videoId', element: <VideoDetail /> },
-    ],
-  },
-]);
-
-function App() {
-  return <RouterProvider router={router} />;
+export default function App() {
+  return <MainProducts />;
 }
-
-export default App;
