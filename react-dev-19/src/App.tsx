01e99e3755c96721b2567889749fb4cc43825cac
devoil <devdevoildev@gmail.com>
2025-04-28 17:24:14 +0900

    feat: [react-19] component 간 데이터 공유

--- react-dev-19/src/App.tsx
+++ react-dev-19/src/App.tsx
@@ -1,20 +1,26 @@
 import { useState } from 'react';
 
-const MyButton = () => {
+type MyButtonProps = {
+  count: number;
+  onClick: () => void;
+};
+
+const MyButton = ({ count, onClick }: MyButtonProps) => {
+  return <button onClick={onClick}>Clicked {count} times</button>;
+};
+
+export const App = () => {
   const [count, setCount] = useState<number>(0);
 
   const handleClick = () => {
     setCount(count + 1);
   };
 
-  return <button onClick={handleClick}>Clicked {count} times</button>;
-};
-
-export const App = () => {
   return (
     <>
-      <MyButton />
-      <MyButton />
+      <h1>Counters that update together</h1>
+      <MyButton count={count} onClick={handleClick} />
+      <MyButton count={count} onClick={handleClick} />
     </>
   );
 };
