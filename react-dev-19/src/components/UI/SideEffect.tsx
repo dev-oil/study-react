d02f10febbbd61d7dd77aa4c35f580f43136faa0
devoil <devdevoildev@gmail.com>
2025-05-08 17:33:40 +0900

    fix: [react-19] side effect 예제 순수하게 변경

--- react-dev-19/src/components/UI/SideEffect.tsx
+++ react-dev-19/src/components/UI/SideEffect.tsx
@@ -1,18 +1,38 @@
-let guest: number = 0;
+// let guest: number = 0;
 
-const Cup = () => {
-  // 나쁜 지점: 이미 존재했던 변수를 변경하고 있습니다!
-  guest = guest + 1;
+// const Cup = () => {
+//   // 나쁜 지점: 이미 존재했던 변수를 변경하고 있습니다!
+//   guest = guest + 1;
 
+//   return <h2>Tea cup for guest #{guest}</h2>;
+// };
+
+// export const SideEffect = () => {
+//   return (
+//     <section className='section'>
+//       <Cup />
+//       <Cup />
+//       <Cup />
+//     </section>
+//   );
+// };
+
+// 고칠 수 있다면?
+type CupProps = {
+  guest: number;
+};
+
+// 오직 guest 프로퍼티에만 의존하기 때문에 컴포넌트는 순수함
+const Cup = ({ guest }: CupProps) => {
   return <h2>Tea cup for guest #{guest}</h2>;
 };
 
 export const SideEffect = () => {
   return (
     <section className='section'>
-      <Cup />
-      <Cup />
-      <Cup />
+      <Cup guest={1} />
+      <Cup guest={7} />
+      <Cup guest={0} />
     </section>
   );
 };
