63700181623a84df2ca1f2a14ca43a994a4d3838
devoil <devdevoildev@gmail.com>
2025-09-20 14:45:15 +0900

    chore: [react-19] clock 파일 이동

--- react-dev-19/src/components/Interactivity/Rendering/DomCommit.tsx
+++ react-dev-19/src/components/Interactivity/Rendering/DomCommit.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Clock } from '../../../pages/Clock';
+import { Clock } from './Clock';
 
 /**
  * ==========================================
